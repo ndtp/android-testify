058817b98c9e1e394b19c330255382ea240181b9
Daniel Jette <daniel.jette@shopify.com>
2019-04-07 15:21:52 -0400

    GitHub pages initial commit

--- script/update_index.sh
+++ script/update_index.sh
@@ -3,6 +3,9 @@
 BASEDIR=$(dirname "$0")
 INDEX_FILE="${BASEDIR}/../index.md"
 
+git checkout master "${BASEDIR}/../README.md"
+git checkout master "${BASEDIR}/../Plugin/README.md"
+
 echo "---" > $INDEX_FILE
 echo "layout: index" >> $INDEX_FILE
 echo "---" >> $INDEX_FILE
