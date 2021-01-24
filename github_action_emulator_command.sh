04ae612f0616ad20f9caf2afde6525d5c7c07af5
Daniel Jette <daniel.jette@neofinancial.com>
2021-01-24 12:59:46 -0500

    Add a retry mechanism for CI

---
+++ github_action_emulator_command.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+GRADLE_COMMAND=$1
+ADB="/Users/runner/Library/Android/sdk/platform-tools/adb"
+
+function reset_emulator() {
+	echo "Attempt to close any system dialogs"
+	$ADB shell am broadcast -a android.intent.action.CLOSE_SYSTEM_DIALOGS
+
+	echo "Send keystroke Arrow Right"
+	sleep 3; $ADB shell input keyevent 22
+	echo "Send keystroke Arrow Right again"
+	sleep 3; $ADB shell input keyevent 22
+	echo "Send keystroke Enter to press a button on the dialog"
+	sleep 3; $ADB shell input keyevent 66
+
+	echo "Lock orientation"
+	$ADB shell settings put system accelerometer_rotation 1
+}
+
+reset_emulator
+
+./gradlew $GRADLE_COMMAND > gradle.logs
+
+cat gradle.logs
+
+MATCHES=`grep --color=no waitForRootToBeReady gradle.logs | wc -l`
+
+if [ MATCHES > 0 ]
+then
+	echo -e "\n\n * Error: Failed with waitForRootToBeReady"
+	echo ""
+	echo "Retrying $GRADLE_COMMAND"
+
+	reset_emulator
+
+	./gradlew $GRADLE_COMMAND
+fi
+
+exit 0
--- sample_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/Users/runner/Library/Android/sdk/platform-tools/adb shell settings put system accelerometer_rotation 1
-./gradlew Sample:testifyKey Sample:screenshotTest
