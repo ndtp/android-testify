eb87f5779ed2d6d9a262239bf35de39ac8c124a2
Daniel Jette <daniel.jette@neofinancial.com>
2021-03-03 09:45:32 -0500

    Add some notes

--- Library/src/main/java/com/shopify/testify/internal/processor/BitmapExtentions.kt
+++ Library/src/main/java/com/shopify/testify/internal/processor/BitmapExtentions.kt
@@ -10,3 +10,29 @@ fun FastPixelProcessor.TransformResult.createBitmap(): Bitmap {
         Bitmap.Config.ARGB_8888
     )
 }
+
+//suspend fun <A, B> Iterable<A>.pmap(f: suspend (A) -> B): List<B> = coroutineScope {
+//    map { async { f(it) } }.awaitAll()
+//}
+
+//val numberOfCores = Runtime.getRuntime().availableProcessors()
+//val executorDispatcher: ExecutorCoroutineDispatcher =
+//    Executors.newFixedThreadPool(numberOfCores ).asCoroutineDispatcher()
+
+
+//inline fun <T, R> Iterable<T>.parallelTransform(
+//    dispatcher: ExecutorDispatcher,
+//    crossinline transform: (T) -> R
+//): Flow<R> = channelFlow {
+//
+//    val items: Iterable<T> = this@parallelTransform
+//    val channelFlowScope: ProducerScope<R> = this@channelFlow
+//
+//    launch(dispatcher) {
+//        items.forEach {item ->
+//            launch {
+//                channelFlowScope.send(transform(item))
+//            }
+//        }
+//    }
+//}
