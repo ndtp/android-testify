1af9dd4fad530486df6cfde494f96d7b34fb077a
Daniel Jette <daniel.jette@neofinancial.com>
2022-03-14 17:43:18 -0400

    Move Gradle Plugin to dev.testify package

---
+++ Plugins/Gradle/src/main/kotlin/dev/testify/TestifyPlugin.kt
@@ -0,0 +1,113 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 ndtp
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package dev.testify
+
+import dev.testify.TestifyPlugin.Companion.EVALUATED_SETTINGS
+import dev.testify.internal.Adb
+import dev.testify.tasks.main.ScreenshotClearTask
+import dev.testify.tasks.main.ScreenshotPullTask
+import dev.testify.tasks.main.ScreenshotRecordTask
+import dev.testify.tasks.main.ScreenshotTestTask
+import dev.testify.tasks.utility.DeviceKeyTask
+import dev.testify.tasks.utility.DevicesTask
+import dev.testify.tasks.utility.DisableSoftKeyboardTask
+import dev.testify.tasks.utility.GenerateDiffImagesTask
+import dev.testify.tasks.utility.HidePasswordsTasks
+import dev.testify.tasks.utility.ImageMagickTask
+import dev.testify.tasks.utility.LocaleTask
+import dev.testify.tasks.utility.RemoveDiffImagesTask
+import dev.testify.tasks.report.ReportPullTask
+import dev.testify.tasks.report.ReportShowTask
+import dev.testify.tasks.utility.SettingsTask
+import dev.testify.tasks.utility.TimeZoneTask
+import dev.testify.tasks.utility.VersionTask
+import org.gradle.api.Action
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class TestifyPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+
+        with(project) {
+            extensions.create(TestifyExtension.NAME, TestifyExtension::class.java)
+            createTasks()
+            afterEvaluate(AfterEvaluate)
+        }
+    }
+
+    private object AfterEvaluate : Action<Project> {
+        override fun execute(project: Project) {
+            project.validateExtension()
+            val settings = TestifySettings.create(project)
+            settings.validate()
+            project.extensions.add(EVALUATED_SETTINGS, settings)
+
+            project.addDependencies()
+
+            if (settings.autoImplementLibrary) {
+                val version = javaClass.getPackage().implementationVersion
+                project.dependencies.add("androidTestImplementation", "com.shopify.testify:testify:$version")
+            }
+
+            Adb.init(project)
+        }
+
+        private fun Project.addDependencies() {
+            ScreenshotTestTask.setDependencies(ScreenshotTestTask.Companion, project = this)
+            ScreenshotRecordTask.setDependencies(ScreenshotRecordTask.Companion, project = this)
+            GenerateDiffImagesTask.setDependencies(GenerateDiffImagesTask.Companion, project = this)
+        }
+    }
+
+    private fun Project.createTasks() {
+        tasks.create(DeviceKeyTask.taskName(), DeviceKeyTask::class.java)
+        tasks.create(DevicesTask.taskName(), DevicesTask::class.java)
+        tasks.create(DisableSoftKeyboardTask.taskName(), DisableSoftKeyboardTask::class.java)
+        tasks.create(GenerateDiffImagesTask.taskName(), GenerateDiffImagesTask::class.java)
+        tasks.create(HidePasswordsTasks.taskName(), HidePasswordsTasks::class.java)
+        tasks.create(ImageMagickTask.taskName(), ImageMagickTask::class.java)
+        tasks.create(LocaleTask.taskName(), LocaleTask::class.java)
+        tasks.create(ReportPullTask.taskName(), ReportPullTask::class.java)
+        tasks.create(ReportShowTask.taskName(), ReportShowTask::class.java)
+        tasks.create(RemoveDiffImagesTask.taskName(), RemoveDiffImagesTask::class.java)
+        tasks.create(ScreenshotClearTask.taskName(), ScreenshotClearTask::class.java)
+        tasks.create(ScreenshotPullTask.taskName(), ScreenshotPullTask::class.java)
+        tasks.create(ScreenshotRecordTask.taskName(), ScreenshotRecordTask::class.java)
+        tasks.create(ScreenshotTestTask.taskName(), ScreenshotTestTask::class.java)
+        tasks.create(SettingsTask.taskName(), SettingsTask::class.java)
+        tasks.create(TimeZoneTask.taskName(), TimeZoneTask::class.java)
+        tasks.create(VersionTask.taskName(), VersionTask::class.java)
+    }
+
+    companion object {
+        const val EVALUATED_SETTINGS = "testify_evaluated_settings"
+    }
+}
+
+internal val Project.testifySettings: TestifySettings
+    get() {
+        return this.extensions.getByName(EVALUATED_SETTINGS) as TestifySettings
+    }
---
+++ Plugins/Gradle/src/main/kotlin/dev/testify/tasks/main/ScreenshotTestTask.kt
@@ -0,0 +1,172 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 ndtp
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package dev.testify.tasks.main
+
+import dev.testify.TestifySettings
+import dev.testify.internal.Adb
+import dev.testify.internal.AdbParam
+import dev.testify.internal.AnsiFormat
+import dev.testify.internal.StreamData.ConsoleStream
+import dev.testify.internal.TestOptionsBuilder
+import dev.testify.internal.fromEnv
+import dev.testify.internal.println
+import dev.testify.tasks.internal.TaskDependencyProvider
+import dev.testify.tasks.internal.TaskNameProvider
+import dev.testify.tasks.internal.TestifyDefaultTask
+import dev.testify.tasks.utility.DisableSoftKeyboardTask
+import dev.testify.tasks.utility.HidePasswordsTasks
+import dev.testify.tasks.utility.LocaleTask
+import dev.testify.tasks.utility.TimeZoneTask
+import dev.testify.testifySettings
+import org.gradle.api.Project
+import org.gradle.api.tasks.Internal
+
+open class ScreenshotTestTask : TestifyDefaultTask() {
+
+    @get:Internal
+    open val isRecordMode: Boolean = false
+
+    override fun getDescription() = "Run the Testify screenshot tests"
+
+    private val settings: TestifySettings
+        get() = project.testifySettings
+
+    private val testTarget: AdbParam?
+        get() {
+            val testClass = project.properties["testClass"] as String?
+            val testName = project.properties["testName"] as String?
+
+            return if (testClass != null && testClass.isNotEmpty()) {
+                AdbParam("class", "$testClass${if (testName != null && testName.isNotEmpty()) "#$testName" else ""}")
+            } else {
+                null
+            }
+        }
+
+    private val shardParams: Set<AdbParam>
+        get() {
+            val shardIndex = project.properties["shardIndex"] as Int?
+            val shardCount = project.properties["shardCount"] as Int?
+
+            val params = HashSet<AdbParam>()
+            if (shardCount != null && shardIndex != null) {
+                println("  Running test shard $shardIndex of $shardCount...")
+                params.add(AdbParam("numShards", "$shardCount"))
+                params.add(AdbParam("shardIndex", "$shardIndex"))
+            }
+            return params
+        }
+
+    private val runtimeParams: List<AdbParam>
+        get() {
+            val params = ArrayList<AdbParam>()
+
+            val useSdCard = "TESTIFY_USE_SDCARD".fromEnv(settings.useSdCard)
+            val outputFormat = "TESTIFY_OUTPUT_FORMAT".fromEnv(settings.outputFileNameFormat)
+
+            if (useSdCard)
+                params.add(AdbParam("useSdCard", "true"))
+
+            if (outputFormat?.isNotEmpty() == true)
+                params.add(AdbParam("outputFileNameFormat", outputFormat))
+
+            if (settings.moduleName.isNotEmpty())
+                params.add(AdbParam("moduleName", settings.moduleName))
+
+            if (isRecordMode)
+                params.add(AdbParam("isRecordMode", "true"))
+
+            return params
+        }
+
+    private val annotation = AdbParam("annotation", "com.shopify.testify.annotation.ScreenshotInstrumentation")
+
+    override fun taskAction() {
+
+        if (isRecordMode) {
+            val clearTask = project.tasks.getByName(ScreenshotClearTask.taskName()) as ScreenshotClearTask
+            clearTask.taskAction()
+        }
+
+        val testOptions = TestOptionsBuilder()
+        testOptions
+            .addAll(shardParams)
+            .add(testTarget)
+            .addAll(runtimeParams)
+            .add(annotation)
+
+        val log = Adb()
+            .shell()
+            .argument("am")
+            .argument("instrument")
+            .testOptions(testOptions)
+            .argument("-w")
+            .argument("${settings.testPackageId}/${settings.testRunner}")
+            .stream(ConsoleStream)
+            .execute()
+
+        if (!isRecordMode &&
+            (log.contains("FAILURES!!!") ||
+                log.contains("INSTRUMENTATION_CODE: 0") ||
+                log.contains("Process crashed while executing"))
+        ) {
+            println(AnsiFormat.Red, "SCREENSHOT TESTS HAVE FAILED!!!")
+            throw RuntimeException("Screenshot tests have failed")
+        }
+
+        if (isRecordMode) {
+            val pullTask = project.tasks.getByName(ScreenshotPullTask.taskName()) as ScreenshotPullTask
+            pullTask.taskAction()
+        }
+    }
+
+    companion object : TaskNameProvider, TaskDependencyProvider {
+        override fun taskName() = "screenshotTest"
+
+        override fun setDependencies(taskNameProvider: TaskNameProvider, project: Project) {
+            val task = project.tasks.getByName(taskNameProvider.taskName())
+
+            task.dependsOn(
+                HidePasswordsTasks.taskName(),
+                DisableSoftKeyboardTask.taskName(),
+                LocaleTask.taskName(),
+                TimeZoneTask.taskName()
+            )
+
+            val settings = project.testifySettings
+            val installDebugAndroidTestTask =
+                project.tasks.findByPath(":${settings.moduleName}:${settings.installAndroidTestTask}")
+            if (installDebugAndroidTestTask != null) {
+                task.dependsOn(installDebugAndroidTestTask)
+            }
+
+            val installDebugTask = project.tasks.findByPath(
+                ":${settings.moduleName}:${settings.installTask}"
+            )
+            if (installDebugTask != null) {
+                task.dependsOn(installDebugTask)
+            }
+        }
+    }
+}
