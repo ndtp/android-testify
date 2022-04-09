535750052454d5ab7855036a53e0a4bd3136ac00
Daniel Jette <daniel.jette@neofinancial.com>
2022-04-09 17:28:39 -0400

    Update copyright to for original work

--- Plugins/IntelliJ/src/main/kotlin/com/shopify/testify/actions/screenshot/ScreenshotTestAction.kt
+++ Plugins/IntelliJ/src/main/kotlin/com/shopify/testify/actions/screenshot/ScreenshotTestAction.kt
@@ -1,7 +1,8 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2020 Shopify Inc.
+ * Modified work copyright (c) 2022 ndtp
+ * Original work copyright (c) 2020 Shopify Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
