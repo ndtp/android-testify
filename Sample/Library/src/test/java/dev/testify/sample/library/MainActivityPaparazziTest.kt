0a21241f0ff47982513107d174c92e880c119e75
Daniel Jette <daniel.jette@neofinancial.com>
2022-05-13 09:14:09 -0400

    Paparazzi works

---
+++ Sample/Library/src/test/java/dev/testify/sample/library/MainActivityPaparazziTest.kt
@@ -0,0 +1,68 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Modified work copyright (c) 2022 ndtp
+ * Original work copyright (c) 2019 Shopify Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package dev.testify.sample.library
+
+import android.view.View
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityPaparazziTest {
+
+//    @get:Rule
+//    val paparazzi = Paparazzi(
+//        deviceConfig = PIXEL_3A,
+//        theme = "android:Theme.Material.Light.NoActionBar" // @style/AppTheme
+//    )
+
+    @Test
+    fun default() {
+//        val view = paparazzi.inflate<View>(R.layout.activity_main)
+//        paparazzi.snapshot(view)
+    }
+}
+
+/*
+
+684ms
+
+    @TestifyLayout(R.layout.view_client_details)
+    @ScreenshotInstrumentation
+    @Test
+    fun default() {
+        rule
+            .setViewModifications { harnessRoot ->
+                val viewState = ClientDetailsViewState(
+                    name = "Testify Test",
+                    avatar = R.drawable.avatar1,
+                    heading = "This is the heading",
+                    address = "1 Address Street\nCity, State, Country\nZ1PC0D3",
+                    phoneNumber = "1-234-567-8910"
+                )
+                val view = harnessRoot.getChildAt(0) as ClientDetailsView
+                view.render(viewState)
+                rule.activity.title = viewState.name
+            }.assertSame()
+    }
+ */
