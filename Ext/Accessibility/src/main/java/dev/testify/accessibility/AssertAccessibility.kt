92f6368242d3cf774c48793c62997d9f1ed35db8
Daniel Jette <jette.daniel@gmail.com>
2022-05-31 17:21:33 -0400

    Implement accessibility checks

---
+++ Ext/Accessibility/src/main/java/dev/testify/accessibility/AssertAccessibility.kt
@@ -0,0 +1,56 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 ndtp
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package dev.testify.accessibility
+
+import android.app.Activity
+import android.view.View
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import dev.testify.ScreenshotRule
+import dev.testify.accessibility.exception.AccessibilityErrorsException
+import dev.testify.accessibility.exception.MalformedBaselineException
+import dev.testify.accessibility.internal.AccessibilityScreenshotLifecycleObserver
+
+/**
+ * To help people with disabilities access Android apps, developers of those apps need to consider how their apps will
+ * be presented to accessibility services. Some good practices can be checked by automated tools, such as if a View has
+ * a contentDescription. Other rules require human judgment, such as whether or not a contentDescription makes sense to
+ * all users.
+ *
+ * Enabling [assertAccessibility] on [ScreenshotRule] will run the latest set of checks as defined by the Accessibility
+ * Test Framework for Android (https://github.com/google/Accessibility-Test-Framework-for-Android). This library
+ * collects various accessibility-related checks on [View] objects as well as AccessibilityNodeInfo objects (which the
+ * Android framework derives from Views and sends to AccessibilityServices).
+ *
+ * @see https://developer.android.com/training/testing/espresso/accessibility-checking
+ *
+ * All elements within the hierarchy defined by [android.R.id.content] will be available to the check.
+ *
+ * @throws [AccessibilityErrorsException] - Errors have been found in the hierarchy.
+ * @throws [MalformedBaselineException] - The baseline json file is corrupt or cannot be loaded.
+ */
+fun <T : Activity> ScreenshotRule<T>.assertAccessibility(): ScreenshotRule<T> {
+    val observer = AccessibilityScreenshotLifecycleObserver(getInstrumentation().context)
+    this.addScreenshotObserver(observer)
+    return this
+}
---
+++ Ext/Accessibility/src/main/java/dev/testify/accessibility/internal/AccessibilityScreenshotLifecycleObserver.kt
@@ -0,0 +1,101 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 ndtp
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package dev.testify.accessibility.internal
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Bitmap
+import android.view.View
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckPreset.LATEST
+import com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckPreset.getAccessibilityHierarchyChecksForPreset
+import com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckResult.AccessibilityCheckResultType.NOT_RUN
+import com.google.android.apps.common.testing.accessibility.framework.AccessibilityHierarchyCheckResult
+import com.google.android.apps.common.testing.accessibility.framework.uielement.AccessibilityHierarchyAndroid
+import dev.testify.ScreenshotLifecycle
+import dev.testify.accessibility.exception.AccessibilityErrorsException
+import dev.testify.internal.extensions.TestInstrumentationRegistry
+import dev.testify.internal.extensions.TestInstrumentationRegistry.Companion.instrumentationPrintln
+import dev.testify.internal.extensions.TestInstrumentationRegistry.Companion.isRecordMode
+import dev.testify.internal.extensions.cyan
+import dev.testify.testDescription
+import java.util.Locale
+
+internal class AccessibilityScreenshotLifecycleObserver(private val context: Context) : ScreenshotLifecycle {
+
+    /**
+     * The screenshot is taken after the activity is fully loaded and rendered. This provides an opportune moment to run
+     * the accessibility checks.
+     *
+     * Here, we run the checks and compare the results to the baseline json.
+     */
+    override fun afterScreenshot(activity: Activity, currentBitmap: Bitmap?) {
+        super.afterScreenshot(activity, currentBitmap)
+
+        val results = runChecksAndSaveBaseline(activity)
+        if (loadBaselineAndCompare(results).not()) {
+            val description = getInstrumentation().testDescription
+            if (isRecordMode) {
+                instrumentationPrintln("\n\t✓ " + "Recording accessibility baseline for ${description.name}".cyan())
+            } else {
+                throw AccessibilityErrorsException(
+                    results,
+                    activity,
+                    description,
+                    TestInstrumentationRegistry.getModuleName()
+                )
+            }
+        }
+    }
+
+    private fun runChecksAndSaveBaseline(activity: Activity): CheckResults {
+        return runAccessibilityChecks(activity).also {
+            writeAccessibilityCheckResults(it)
+        }
+    }
+
+    private fun loadBaselineAndCompare(currentResults: CheckResults): Boolean {
+        val baselineResults = readAccessibilityCheckResults(context)
+        return (baselineResults == currentResults)
+    }
+
+    private fun runAccessibilityChecks(activity: Activity): CheckResults {
+        val view = activity.findViewById<View>(android.R.id.content)
+        val checks = getAccessibilityHierarchyChecksForPreset(LATEST)
+        val hierarchy = AccessibilityHierarchyAndroid.newBuilder(view).build()
+        val results = checks.flatMap { it.runCheckOnHierarchy(hierarchy) }
+        return CheckResults(results.filter { it.type != NOT_RUN }.map { it.toCheckResult() })
+    }
+
+    private fun AccessibilityHierarchyCheckResult.toCheckResult(): CheckResult {
+        return CheckResult(
+            id = this.resultId,
+            type = this.type.name,
+            check = this.sourceCheckClass.simpleName,
+            elementClass = this.element?.className.toString(),
+            resourceName = this.element?.resourceName.toString(),
+            message = this.getMessage(Locale.getDefault()).toString()
+        )
+    }
+}
