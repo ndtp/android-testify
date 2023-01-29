f27f1b1f74887293015e7497af0218e948783cfd
Daniel Jette <jette.daniel@gmail.com>
2023-01-29 14:37:45 -0500

    Update Intellij plugin CI

--- Plugins/IntelliJ/build.gradle.kts
+++ Plugins/IntelliJ/build.gradle.kts
@@ -1,8 +1,9 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 plugins {
-  id("org.jetbrains.kotlin.jvm") version "1.6.10"
-  id("org.jetbrains.intellij") version "1.12.0"
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    id("org.jetbrains.intellij") version "1.12.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
 
 fun properties(key: String) = project.findProperty(key).toString()
@@ -11,36 +12,36 @@ group = project.property("pluginGroup") as String
 version = properties("version")
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-  version.set(properties("InstrumentCodeVersion"))
-  type.set(properties("platformType"))
+    version.set(properties("InstrumentCodeVersion"))
+    type.set(properties("platformType"))
 
-  plugins.add("Kotlin")
-  plugins.add("gradle")
+    plugins.add("Kotlin")
+    plugins.add("gradle")
 }
 
 tasks {
-  buildSearchableOptions {
-    enabled = false
-  }
+    buildSearchableOptions {
+        enabled = false
+    }
 
-  patchPluginXml {
-    version.set("${project.version}")
-    sinceBuild.set(properties("pluginSinceBuild"))
-    untilBuild.set(properties("pluginUntilBuild"))
-  }
+    patchPluginXml {
+        version.set("${project.version}")
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
+    }
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
@@ -50,9 +51,9 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 tasks {
-  runIde {
-    // Absolute path to installed target 3.5 Android Studio to use as
-    // IDE Development Instance (the "Contents" directory is macOS specific):
-    ideDir.set(file("/Applications/Android Studio Preview 2022.3.1.2.app/Contents"))
-  }
+    runIde {
+        // Absolute path to installed target 3.5 Android Studio to use as
+        // IDE Development Instance (the "Contents" directory is macOS specific):
+        ideDir.set(file("/Applications/Android Studio Preview 2022.3.1.2.app/Contents"))
+    }
 }
