0b0ca51bdc5013900d025e2e9e8c318beb025168
Daniel Jette <jette.daniel@gmail.com>
2023-02-21 13:45:22 -0500

    Enable Algolia search

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -69,6 +69,35 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'HRW7HBODJM',
+
+        // Public API key: it is safe to commit it
+        apiKey: 'cb1ccf3c9d6658b87862a1a8c51aef00',
+
+        indexName: 'android-testify',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+          // from: '/docs/', // or as RegExp: /\/docs\//
+          // to: '/',
+        // },
+
+        // Optional: Algolia search parameters
+        // searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        // searchPagePath: 'search',
+
+        //... other Algolia params
+      },
       navbar: {
         title: 'Android Testify',
         logo: {
