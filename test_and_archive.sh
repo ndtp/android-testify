b6e340b7461f4bd667a13d02d4f0b81b5e1f380d
Daniel Jette <jette.daniel@gmail.com>
2023-05-27 17:18:25 -0400

    ISSUE-142: Run Flix screenshot tests on CI

--- test_and_archive.sh
+++ test_and_archive.sh
@@ -7,3 +7,11 @@
 mkdir -p ./output/
 
 git status -s --porcelain | grep --color=no "^ M.*png$" | sed s/" M "/""/g | xargs -I {} cp {} ./output/
+
+./github_action_emulator_command.sh FlixSample:screenshotTest
+
+./gradlew FlixSample:screenshotPull
+
+mkdir -p ./output/
+
+git status -s --porcelain | grep --color=no "^ M.*png$" | sed s/" M "/""/g | xargs -I {} cp {} ./output/
