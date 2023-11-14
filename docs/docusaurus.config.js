934aad879c22ca2e45f8370b0764cfee4ba0dd48
Daniel Jette <jette.daniel@gmail.com>
2023-11-14 08:56:54 -0500

    Release documentation for 2.0.0

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '2.0.0-beta04',
+              label: '2.0.0',
             },
           },
           sidebarPath: require.resolve('./sidebars.js'),
