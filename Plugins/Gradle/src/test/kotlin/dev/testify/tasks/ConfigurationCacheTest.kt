85f59d9cced5d223864cb8ace2975ffba43319aa
Daniel Jette <jette.daniel@gmail.com>
2023-12-29 11:17:49 -0500

    wip -- more wonkyness

---
+++ Plugins/Gradle/src/test/kotlin/dev/testify/tasks/ConfigurationCacheTest.kt
@@ -0,0 +1,97 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023 ndtp
+  *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package dev.testify.tasks
+
+import com.google.common.truth.Truth.assertThat
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.io.TempDir
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.io.File
+
+class ConfigurationCacheTest {
+
+//    @BeforeEach
+//    fun setup() {
+//        settingsFile = File(testProjectDir, "settings.gradle")
+//        buildFile = File(testProjectDir, "build.gradle").apply {
+//            writeText("plugins { id 'dev.testify' }")
+//        }
+//    }
+    companion object {
+
+        @field:TempDir
+        @JvmField
+        var testProjectDir: File? = null
+
+        private var settingsFile: File? = null
+        private var buildFile: File? = null
+
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            settingsFile = File(testProjectDir, "settings.gradle")
+            buildFile = File(testProjectDir, "build.gradle").apply {
+                writeText("plugins { id 'dev.testify' }")
+            }
+        }
+    }
+
+    private fun testConfigurationCache(taskName: String): BuildResult =
+        GradleRunner
+            .create()
+            .withProjectDir(File("../.."))
+            .withArguments("--configuration-cache", taskName)
+            .build()
+
+    private fun assertCacheReused(result: BuildResult) =
+        assertThat(result.output).contains("Configuration cache entry reused.")
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "screenshotClear",
+            "screenshotPull",
+            "screenshotRecord",
+            "screenshotTest",
+            "reportPull",
+            "reportShow",
+            "testifyKey",
+            "testifyDevices",
+            "disableSoftKeyboard",
+            "hidePasswords",
+            "deviceLocale",
+            "testifySettings",
+            "deviceTimeZone",
+            "testifyVersion",
+        ]
+    )
+    fun `WHEN task run twice AND configuration cache enabled THEN reuse cache entry`(taskName: String) {
+        testConfigurationCache(taskName)
+        assertCacheReused(testConfigurationCache(taskName))
+    }
+}
