dcd05f8040c9e083f14ad0b2c7788ebe65ccd964
Daniel Jette <jette.daniel@gmail.com>
2026-08-11 12:31:37 -0400

    Added support for Support Android Studio Quail 4 | 2026.1.4 Canary 4 | 261.* (#302)
    * Added support for Support Android Studio Quail 4 | 2026.1.4 Canary 4 | 261.*
    
    * Use ubuntu-resolute-26.04-bitrise-2026-android on Bitrise
    
    * Address PR feedback

--- Plugins/IntelliJ/build.gradle.kts
+++ Plugins/IntelliJ/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        androidStudio(providers.gradleProperty("platformVersion"), useInstaller = true)
+        androidStudio(providers.gradleProperty("platformVersion"))
 
         // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
         bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
